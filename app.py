75caf5b7105eaaa46326837762aa842d135d8b6c
Clément Tailleur <clement.tailleur@qare.io>
2022-09-24 23:01:20 +0200

    test ci/cd commit with fake json data

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World - Sellaci first RUN'
+def main():
+    return {
+        'name': 'Gabriel Jesus',
+        'clubs': [
+            'Palmeiras',
+            'Manchester City',
+            'Arsenal',
+        ],
+        'nationality': 'Brazil'
+    }
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
