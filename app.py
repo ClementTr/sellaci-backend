9378d8a0d10030d860f46c6251933b4dff231998
Clément Tailleur <clement.tailleur@qare.io>
2022-09-30 00:27:59 +0200

    add static fake data

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask
-from data import data
+from static.data.consultations_to_clean import consultations_to_clean
+from flask import Flask, request
+import json
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return [
-        data
-    ]
+    return consultations_to_clean
+
+@app.route('/hospital_data', methods=['GET'])
+def consultations():
+    try:
+        collection = request.args.get('collection')
+        data = []
+        filename = os.path.join(app.static_folder, f'data/{collection}.json')
+        with open(filename) as blog_file:
+            data = json.load(blog_file)
+        return data
+    except:
+        return [{'server-status': 'error'}]
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
