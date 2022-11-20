b4fb37db59977d40d2ae26781c284cdf3803c4d6
Clément Tailleur <clement.tailleur@qare.io>
2022-11-20 23:08:21 +0100

    change env variable

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ PYREBASE_CREDENTIALS = {
 
 FIREBASE_CREDENTIALS = {
     "type": "service_account",
-    "project_id": "sellaci",
+    "project_id": os.environ.get('GOOGLE_PROJECT_ID'),
     "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
     "private_key": os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
     "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
@@ -20,5 +20,5 @@ FIREBASE_CREDENTIALS = {
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-vrooh%40sellaci.iam.gserviceaccount.com"
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-w8zsd%40sellaci-367920.iam.gserviceaccount.com"
 }
